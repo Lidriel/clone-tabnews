397a8401747fa0d5bd5ba9453055b25038aa0d82
Lidriel <105443904+Lidriel@users.noreply.github.com>
2026-05-09 13:35:48 +0000

    Adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <h1>
+            Aline<br />
+            Pitoca Toca Chopper disse:<br />
+            🐱 Miaumãe, você dorme muito 💤
+        </h1>
+    );
+}
+
+export default Home;
